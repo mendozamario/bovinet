ba000175dc22dbb2d9ecb424de28c4a5854e2a3a
Mario Mendoza <menriquemendoza@unicesar.edu.co>
2020-12-17 14:16:35 -0500

    finaliced presentation

--- bovinet/Models/MedicineInputModel.cs
+++ bovinet/Models/MedicineInputModel.cs
@@ -1,15 +1,22 @@
 using System;
 using Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace bovinet.Models 
 {
     public class MedicineInputModel
     {
+        [Required(ErrorMessage = "Code is required")]
         public string Code { get; set; }
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Type is required")]
         public string Type { get; set; }
+        [Required(ErrorMessage = "Expiration date is required")]
         public string ExpirationDate { get; set; }
+        [Required(ErrorMessage = "Unit cost is required")]
         public float UnitCost { get; set; }
+        [Required(ErrorMessage = "Quantity is required")]
         public int Quantity { get; set; }
     }
     public class MedicineViewModel : MedicineInputModel
--- bovinet/Models/OwnerInputModel.cs
+++ bovinet/Models/OwnerInputModel.cs
@@ -1,14 +1,21 @@
 using Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace bovinet.Models
 {
     public class OwnerInputModel
     {
+        [Required(ErrorMessage = "Identification is required")]
         public string Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Mail is required"), RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
         public string Mail { get; set; }
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Phone number is required")]
         public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Type is required")]
         public string Type { get; set; } = "User";
         public string UserId { get; set; }
     }
